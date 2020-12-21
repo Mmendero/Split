2d681e14d1f13d3cfc2d0f995605c2b8db25fab5
Matthew Mendero <matthewmendero@gmail.com>
2020-12-20 23:04:59 -0500

    Implemented Navigation from Input to Output Screens

--- src/navigation/appStack.js
+++ src/navigation/appStack.js
@@ -2,14 +2,14 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
 
 import Split from '../screens/split';
-import Test from '../screens/test';
+import SplitResult from '../screens/split-result';
 
 const screens = {
     Home:{
         screen: Split
     },
     Output:{
-        screen: Test
+        screen: SplitResult
     }
 }
 
