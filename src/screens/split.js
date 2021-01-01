255e6a6b3905a17249a4978ed0dcd37d2ccf89e3
Matthew Mendero <matthewmendero@gmail.com>
2020-12-31 21:23:47 -0500

    Added Basic Unique Key Generator

--- src/screens/split.js
+++ src/screens/split.js
@@ -21,9 +21,9 @@ import Bill from "../components/bill-input";
 //Import Styles
 import styles from '../styles';
 
-//Helper Function
-function uniqueKey(){
-  return "Some Unique Key";
+//Generate Unique Id for Party Members
+function uniqueKey(name){
+  return name + '_key_' + name;
 }
 
 //Input Screen Class
@@ -37,15 +37,15 @@ class Split extends Component {
       bill: '0',
       split: '0',
       party: [
-        {name: 'Matt', key: 'key-1'},
-        {name: 'Sandra', key: 'key-2'},
-        {name: 'Merlinda', key: 'key-3'},
-        {name: 'Rossette', key: 'key-4'},
-        {name: 'Jason', key: 'key-5'},
-        {name: 'Jerry', key: 'key-6'},
-        {name: 'Angel', key: 'key-7'},
-        {name: 'Brandon', key: 'key-8'},
-        {name: 'Jordan', key: 'key-9'},
+        {name: 'Matt', key: uniqueKey('Matt')},
+        {name: 'Sandra', key: uniqueKey('Sandra')},
+        {name: 'Merlinda', key: uniqueKey('Merlinda')},
+        {name: 'Rossette', key: uniqueKey('Rossette')},
+        {name: 'Jason', key: uniqueKey('Jason')},
+        {name: 'Jerry', key: uniqueKey('Jerry')},
+        {name: 'Angel', key: uniqueKey('Angel')},
+        {name: 'Brandon', key: uniqueKey('Brandon')},
+        {name: 'Jordan', key: uniqueKey('Jordan')},
       ]
 
     }
@@ -61,7 +61,7 @@ class Split extends Component {
     if(this.state.party.find(person => person.name == member.name) == null){
       this.setState(() => {
         return{
-          party:[{name: member, key: '0'}, ...this.state.party]
+          party:[{name: member, key: uniqueKey(member)}, ...this.state.party]
         }
       })
     }
@@ -70,7 +70,7 @@ class Split extends Component {
   //Edit member to State party
   editMember = (member) => {
     console.log("Edittable: True");
-    print();
+    console.log(member);
   }
   
   //Even Split Logic
